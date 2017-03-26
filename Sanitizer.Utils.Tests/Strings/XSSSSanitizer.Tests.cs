3e42f51b36d1bf2d2887d94b6b42685b733546e0
Sergio Magaña <sergiotem@gmail.com>
2017-03-27 00:04:47 +0200

    add XSS tests

--- Sanitizer.Utils.Tests/Strings/XSSSSanitizer.Tests.cs
+++ Sanitizer.Utils.Tests/Strings/XSSSSanitizer.Tests.cs
@@ -6,7 +6,9 @@ namespace Sanitizer.Utils.Tests.Strings
     [TestFixture]
     public class XSSSSanitizerTests
     {
+        [TestCase("<img src='javascript:alert(Hack!)'", "[img src='[javascript]:alert(Hack!)'")]
         [TestCase("<script>alert('attacked')</script>", "[script]alert('attacked')[/script]")]
+        [TestCase("< script>alert('attacked')</script>", "[script]alert('attacked')[/script]")]
         public void Test_Sanitize(string value, string expected)
         {
             var sanitizer = new XSSStringSanitizer();
